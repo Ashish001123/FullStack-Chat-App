bf5e402c88bf4f7d8c65cd776a84abeb53fe3d80
Ashish Khatri <ashishkhatri006@gmail.com>
2026-02-16 15:49:53 +0530

    change ai_agent url in backend

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -1,13 +1,46 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from agent import run_agent
+
+# app = FastAPI()
+
+# class Query(BaseModel):
+#     message: str
+
+# @app.post("/chat")
+# def chat(q: Query):
+#     reply = run_agent(q.message)
+#     return {"reply": reply}
+
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from agent import run_agent
+from openai import OpenAI
+import os
 
 app = FastAPI()
 
-class Query(BaseModel):
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+class ChatRequest(BaseModel):
     message: str
 
 @app.post("/chat")
-def chat(q: Query):
-    reply = run_agent(q.message)
-    return {"reply": reply}
+async def chat(req: ChatRequest):
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {"role": "system", "content": "You are AI assistant for Chatty chat app."},
+                {"role": "user", "content": req.message}
+            ],
+        )
+
+        reply = response.choices[0].message.content
+
+        return {"reply": reply}
+
+    except Exception as e:
+        print("AI ERROR:", str(e))
+        return {"reply": "AI service error"}
