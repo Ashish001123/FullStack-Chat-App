f3109303129e06a437d300383dc362870beddd87
Ashish Khatri <ashishkhatri006@gmail.com>
2026-02-16 16:33:11 +0530

    chnaged main.py

--- backend/src/routes/ai.route.js
+++ backend/src/routes/ai.route.js
@@ -1,22 +1,51 @@
+// import express from "express";
+// import axios from "axios";
+
+// const router = express.Router();
+
+// const AI_URL = process.env.AI_URL;
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const { message } = req.body;
+
+//     const aiRes = await axios.post(
+//       `${AI_URL}`,
+//       { message }
+//     );
+
+//     res.json({ reply: aiRes.data.reply });
+//   } catch (err) {
+//     console.error("AI error:", err.message);
+//     res.status(500).json({ error: "AI failed" });
+//   }
+// });
+
+// export default router;
+
+
+
+
 import express from "express";
 import axios from "axios";
 
 const router = express.Router();
 
-const AI_URL = process.env.AI_URL;
+const AI_URL = process.env.AI_URL; 
+
 
 router.post("/", async (req, res) => {
   try {
     const { message } = req.body;
 
-    const aiRes = await axios.post(
-      `${AI_URL}`,
-      { message }
-    );
+   const aiRes = await axios.post(
+  process.env.AI_URL,
+  { message }
+);
 
     res.json({ reply: aiRes.data.reply });
   } catch (err) {
-    console.error("AI error:", err.message);
+    console.error("AI error:", err.response?.data || err.message);
     res.status(500).json({ error: "AI failed" });
   }
 });
