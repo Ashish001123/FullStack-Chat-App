34f0730aaa17ae8d7d007abe603c9590559d0831
Ashish Khatri <ashishkhatri006@gmail.com>
2026-02-16 16:39:30 +0530

    change the sendMessageLogic

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -63,39 +63,78 @@ export const getMessages = async (req, res) => {
 };
 
 
+// export const sendMessage = async (req, res) => {
+//   try {
+//     const { text, image } = req.body;
+//     const { id: receiverId } = req.params;
+//     const senderId = req.user._id;
+
+//     let imageUrl;
+//     if (image) {
+//       const uploadResponse = await cloudinary.uploader.upload(image);
+//       imageUrl = uploadResponse.secure_url;
+//     }
+
+//     const newMessage = new Message({
+//       senderId,
+//       receiverId,
+//       text,
+//       image: imageUrl,
+//       isRead: false,
+//     });
+
+//     await newMessage.save();
+
+//     const receiverSocketId = getReceiverSocketId(receiverId);
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit("newMessage", newMessage);
+//     }
+
+//     res.status(201).json(newMessage);
+//   } catch (error) {
+//     console.log("Error in sendMessage controller: ", error.message);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+
 export const sendMessage = async (req, res) => {
   try {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
-    let imageUrl;
-    if (image) {
-      const uploadResponse = await cloudinary.uploader.upload(image);
-      imageUrl = uploadResponse.secure_url;
+    if (receiverId === "ai_assistant") {
+      const aiReply = "Hello 👋 I'm your AI assistant"; 
+
+      const aiMessage = new Message({
+        senderId: senderId,
+        receiverId: senderId, 
+        text: aiReply,
+        isRead: true,
+      });
+
+      await aiMessage.save();
+
+      return res.status(201).json(aiMessage);
     }
 
     const newMessage = new Message({
       senderId,
       receiverId,
       text,
-      image: imageUrl,
       isRead: false,
     });
 
     await newMessage.save();
 
-    const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
-
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message);
+    console.log("Error in sendMessage:", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
 export const markMessagesAsRead = async (req, res) => {
   try {
     const myId = req.user._id;
