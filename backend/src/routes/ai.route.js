66d4aba6bbc62723c8e0074cb4c4331ec9d9cda3
Ashish Khatri <ashishkhatri006@gmail.com>
2026-02-16 15:32:20 +0530

    change ai_agent url in backend

--- backend/src/routes/ai.route.js
+++ backend/src/routes/ai.route.js
@@ -1,19 +1,46 @@
+// import express from "express";
+// import axios from "axios";
+
+// const router = express.Router();
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const { message } = req.body;
+
+//     const aiRes = await axios.post(
+//       "http://localhost:8000/chat",
+//       { message }
+//     );
+
+//     res.json({ reply: aiRes.data.reply });
+//   } catch (err) {
+//     res.status(500).json({ error: "AI failed" });
+//   }
+// });
+
+// export default router;
+
+
+
 import express from "express";
 import axios from "axios";
 
 const router = express.Router();
+
+const AI_URL = process.env.AI_URL;
 
 router.post("/", async (req, res) => {
   try {
     const { message } = req.body;
 
     const aiRes = await axios.post(
-      "http://localhost:8000/chat",
+      `${AI_URL}/chat`,
       { message }
     );
 
     res.json({ reply: aiRes.data.reply });
   } catch (err) {
+    console.error("AI error:", err.message);
     res.status(500).json({ error: "AI failed" });
   }
 });
