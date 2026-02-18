a9fdb981e2603a089f5f6935edc5ec435ab3b568
Ashish Khatri <ashishkhatri006@gmail.com>
2026-02-19 01:31:45 +0530

    changed ai response

--- frontend/src/store/ai.store.js
+++ frontend/src/store/ai.store.js
@@ -6,28 +6,6 @@ const useAIStore = create((set, get) => ({
   messages: [],
   isLoading: false,
 
-  // sendMessage: async (text) => {
-  //   const authUser = useAuthStore.getState().authUser;
-
-  //   if (!authUser) return;
-
-  //   const userMsg = { role: "user", text };
-
-  //   set((s) => ({
-  //     messages: [...s.messages, userMsg],
-  //     isLoading: true,
-  //   }));
-
-  //   const response = await axiosInstance.post("/ai", {
-  //     messages: [...get().messages, { role: "user", content: text }],
-  //   });
-  //   const aiMsg = { role: "ai", text: response.data.reply };
-
-  //   set((s) => ({
-  //     messages: [...s.messages, aiMsg],
-  //     isLoading: false,
-  //   }));
-  // },
   sendMessage: async (text) => {
   const authUser = useAuthStore.getState().authUser;
   if (!authUser) return;
