dc02b6467e9a0119feb91c1b3466707458e25f45
Ashish Khatri <ashishkhatri006@gmail.com>
2026-04-01 11:24:33 +0530

    fixed ai response

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,21 +1,12 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === "development"
-//       ? "http://localhost:5001/api"
-//       : "/api",
-//   withCredentials: true,
-// });
-
-
-
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5001/api";
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
   withCredentials: true,
 });
+
+
+
